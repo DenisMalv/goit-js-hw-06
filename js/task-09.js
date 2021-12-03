85ecfe86e004a28a883942fa53e62203ca844dda
DenisMalv <kissed03@gmail.com>
2021-12-03 22:05:02 +0200

    Update task 9.js

--- js/task-09.js
+++ js/task-09.js
@@ -2,24 +2,35 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+function rgb2hex(rgb) {
+ var hexDigits = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"];
+ rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+ function hex(x) {
+  return isNaN(x) ? "00" : hexDigits[(x - x % 16) / 16] + hexDigits[x % 16];
+ }
+ return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+}
+
 const body = document.body
 const span = document.querySelector('.color')
 const button = document.querySelector('.change-color')
-console.log(body.style.background.toString(16))
-console.log(body)
-console.log(span)
-console.log(button)
-console.log(getRandomHexColor())
-console.dir(body)
+
 
 button.addEventListener('click', () => {
  span.textContent = getRandomHexColor()
   
   body.style.background = `${span.textContent}`
-  console.dir(span)
-console.log(body.style.backgroundColor)
+console.log(rgb2hex(body.style.background))
 })
 
+
+
+// console.log(body.style.background.toString(16))
+// console.log(body)
+// console.log(span)
+// console.log(button)
+// console.log(getRandomHexColor())
+// console.dir(body)
 
 // function rgbToHex(R,G,B) {return toHex(R)+toHex(G)+toHex(B)}
 // function toHex(n) {
@@ -30,14 +41,7 @@ console.log(body.style.backgroundColor)
 //       + "0123456789ABCDEF".charAt(n%16);
 // }
 
-// function rgb2hex(rgb) {
-//  var hexDigits = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"];
-//  rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-//  function hex(x) {
-//   return isNaN(x) ? "00" : hexDigits[(x - x % 16) / 16] + hexDigits[x % 16];
-//  }
-//  return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
-// }
+
 
 // function rgb2hex(rgb) {
 //  var hexDigits = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"];
