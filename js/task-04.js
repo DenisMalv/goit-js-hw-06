cb63e5941d854716b56dd1e7be1e3c983cbf18c9
DenisMalv <kissed03@gmail.com>
2021-12-03 22:09:59 +0200

    update task 4

--- js/task-04.js
+++ js/task-04.js
@@ -3,5 +3,5 @@ const increment = document.querySelector('button[data-action="increment"]')
 const value = document.querySelector('#value')
 let counterValue = 0
  
-decrement.addEventListener('click',() => value.textContent =  counterValue+=+1)
-increment.addEventListener('click',() => value.textContent =  counterValue+=-1)
+decrement.addEventListener('click',() => value.textContent =  counterValue+=-1)
+increment.addEventListener('click',() => value.textContent =  counterValue+=+1)
